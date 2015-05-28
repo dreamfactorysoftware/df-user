596c461ef77738827aaa634b36a532dc77b2a5d9
Arif Islam <arif@dreamfactory.com>
2015-05-28 14:04:33 -0400

    user resource updates

--- src/Resources/System/User.php
+++ src/Resources/System/User.php
@@ -20,9 +20,48 @@
 
 namespace DreamFactory\Rave\User\Resources\System;
 
+use DreamFactory\Library\Utility\ArrayUtils;
+use DreamFactory\Rave\Models\BaseSystemModel;
 use DreamFactory\Rave\Resources\System\BaseSystemResource;
 
 class User extends BaseSystemResource
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSelectionCriteria()
+    {
+        $criteria = parent::getSelectionCriteria();
 
+        $condition = ArrayUtils::get( $criteria, 'condition' );
+
+        if ( !empty( $condition ) )
+        {
+            $condition .= ' AND is_sys_admin = "0" ';
+        }
+        else
+        {
+            $condition = ' is_sys_admin = "0" ';
+        }
+
+        ArrayUtils::set( $criteria, 'condition', $condition );
+
+        return $criteria;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function retrieveById( $id, array $related = [ ] )
+    {
+        /** @var BaseSystemModel $modelClass */
+        $modelClass = $this->model;
+        $criteria = $this->getSelectionCriteria();
+        $fields = ArrayUtils::get( $criteria, 'select' );
+        $model = $modelClass::whereIsSysAdmin( 0 )->with( $related )->find( $id, $fields );
+
+        $data = ( !empty( $model ) ) ? $model->toArray() : [ ];
+
+        return $data;
+    }
 }
