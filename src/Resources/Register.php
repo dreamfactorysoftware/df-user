2c95c010d18a0a45febd70dea6e6479d58e4f562
Lee Hicks <leehicks@dreamfactory.com>
2017-01-09 15:47:54 -0500

    cleanup of MERGE, handled at router/controller level.

--- src/Resources/Register.php
+++ src/Resources/Register.php
@@ -21,7 +21,6 @@ class Register extends BaseRestResource
         $settings = (array)$settings;
         $settings['verbAliases'] = [
             Verbs::PUT   => Verbs::POST,
-            Verbs::MERGE => Verbs::POST,
             Verbs::PATCH => Verbs::POST
         ];
 
